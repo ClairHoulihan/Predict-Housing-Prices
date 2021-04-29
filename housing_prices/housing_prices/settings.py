0bfb3732714c395a054f7c6f1f2a981e733ff83a
ClairHoulihan <clairhoulihan@yahoo.com>
2021-04-29 16:08:46 -0500

    Initial Upload of Housing Data

--- housing_prices/housing_prices/settings.py
+++ housing_prices/housing_prices/settings.py
@@ -22,7 +22,7 @@ STATIC_URL = '/static/'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0!ev1=+q=_h+)h(q=u6%rrf))gmaab7rwqub2qeazewhdkdi%7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
